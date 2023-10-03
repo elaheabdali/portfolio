eec25105641cd52209144ef29bbeb624dbd76247
elaheabdali <ohelahe@gmail.com>
2023-10-03 22:15:25 +0330

    added page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import Intro from "../components/intro";
+import SectionDivider from '@/components/section-divider';
+import Intro from '../components/intro';
+import About from '@/components/about';
+import SectionHeading from '@/components/section-heading';
+import Projects from '@/components/projects';
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center px-4 ">
+    <main className='flex flex-col items-center px-4 '>
       <Intro />
+      <SectionDivider />
+      <About />
+      <Projects />
     </main>
-  )
+  );
 }
