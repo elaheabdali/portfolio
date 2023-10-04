e887ca4e1b230522d360b73ddc93a725be3d632a
elaheabdali <ohelahe@gmail.com>
2023-10-04 19:03:15 +0330

    change projects

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,11 +1,15 @@
+'use client';
 import SectionHeading from './section-heading';
 import { projectsData } from '@/lib/data';
 import Project from './project';
 import React from 'react';
+import { useSectionInView } from '@/lib/hooks';
 
 const Projects = () => {
+  const { ref } = useSectionInView('Projects', 0.5);
+
   return (
-    <section id='projects' className='scroll-mt-28'>
+    <section ref={ref} id='projects' className='scroll-mt-28'>
       <SectionHeading>My Projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
