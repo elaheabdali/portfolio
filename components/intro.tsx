60806be19af79e00d694eb9eb6dcee08ef53234e
elaheabdali <ohelahe@gmail.com>
2023-10-04 19:02:57 +0330

    change intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,10 +5,13 @@ import Link from 'next/link';
 import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
 import { HiDownload } from 'react-icons/hi';
 import { FaGithubSquare } from 'react-icons/fa';
+import { useSectionInView } from '@/lib/hooks';
 
 const Intro = () => {
+  const { ref } = useSectionInView('Home', 0.5);
   return (
     <section
+      ref={ref}
       id='home'
       className='scroll-mt-[100rem] mb-28 max-w-[50rem] text-center
     sm:mb-0'
