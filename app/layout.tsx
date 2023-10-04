3a5ca0c36cf1c43e6e6b204cff5826c808c23f67
elaheabdali <ohelahe@gmail.com>
2023-10-04 19:02:30 +0330

    added new layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/header';
 import './globals.css';
 import { Inter } from 'next/font/google';
+import ActiveSectionContextProvider from '@/context/active-section-context';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -32,8 +33,11 @@ export default function RootLayout({
         blur-[10rem] sm:w-[68.75rem] md:left-[-33rem]
         lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'
         ></div>
-        <Header />
-        {children}
+        
+        <ActiveSectionContextProvider>
+          <Header />
+          {children}
+        </ActiveSectionContextProvider>
       </body>
     </html>
   );
