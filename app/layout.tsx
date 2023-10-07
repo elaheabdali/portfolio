67e4d3e58c4a8f157a496f93779cb46d5f4d1ff2
elaheabdali <ohelahe@gmail.com>
2023-10-07 22:09:30 +0330

    added new layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
-import Header from '@/components/header';
+import Header from '../components/header';
 import './globals.css';
 import { Inter } from 'next/font/google';
-import ActiveSectionContextProvider from '@/context/active-section-context';
+import ActiveSectionContextProvider from '../context/active-section-context';
+import { Toaster } from 'react-hot-toast';
+import Footer from '@components/footer';
+import ThemeSwitch from '../components/theme-switch';
+import ThemeContextProvider from '../context/theme-context';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,24 +24,31 @@ export default function RootLayout({
       <body
         className={`${inter.className}
       bg-gray-50 relative pt-28 text-gray-950
-      sm:pt-36`}
+      sm:pt-36 dark:bg-gray-900 dark:text-gray-50
+      dark:opacity-90`}
       >
         <div
           className='bg-[#fbe2e3] absolute top-[-6rem]
         right-[11rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full
-        blur-[10rem] sm:w-[68.75rem]'
+        blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]'
         ></div>
         <div
           className='bg-[#dbd7fb] absolute top-[-1rem]
         left-[35rem] -z-10 h-[31.25rem] w-[50rem] rounded-full
         blur-[10rem] sm:w-[68.75rem] md:left-[-33rem]
-        lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'
+        lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]
+        dark:bg-[#676394]'
         ></div>
-        
-        <ActiveSectionContextProvider>
-          <Header />
-          {children}
-        </ActiveSectionContextProvider>
+
+        <ThemeContextProvider>
+          <ActiveSectionContextProvider>
+            <Header />
+            {children}
+            <Footer />
+            <Toaster position='top-right' />
+          <ThemeSwitch />
+          </ActiveSectionContextProvider>
+        </ThemeContextProvider>
       </body>
     </html>
   );
