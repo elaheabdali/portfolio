0255e4420d075119e274ebb4dba1f929ed26e62d
elaheabdali <ohelahe@gmail.com>
2023-10-07 22:09:19 +0330

    added send Email

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,42 +1,46 @@
 'use server';
 
-import { validate, getErrorMessage } from '../lib/utils';
-import { Resend } from 'resend';
-import Email from '../email/email';
 import React from 'react';
+import { Resend } from 'resend';
+import { validate, getErrorMessage } from '../lib/utils';
+import ContactFormEmail from '../email/email';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (formData: FormData) => {
-  const message = formData.get('message');
   const senderEmail = formData.get('senderEmail');
+  const message = formData.get('message');
 
   if (!validate(senderEmail, 500)) {
     return {
       error: 'Invalid sender email',
     };
   }
-
   if (!validate(message, 5000)) {
     return {
-      error: 'Invalid message ',
+      error: 'Invalid message',
     };
   }
 
+  let data;
   try {
-    await resend.emails.send({
-      from: 'onboarding@resend.dev',
-      to: 'ohelahe@gmail.com',
-      subject: 'Message sent from contact form',
+    data = await resend.emails.send({
+      from: 'Contact Form <onboarding@resend.dev>',
+      to: 'bytegrad@gmail.com',
+      subject: 'Message from contact form',
       reply_to: senderEmail as string,
-      react: React.createElement(Email, {
+      react: React.createElement(ContactFormEmail, {
         message: message as string,
         senderEmail: senderEmail as string,
       }),
     });
-  } catch (error: any) {
+  } catch (error: unknown) {
     return {
       error: getErrorMessage(error),
     };
   }
+
+  return {
+    data,
+  };
 };
