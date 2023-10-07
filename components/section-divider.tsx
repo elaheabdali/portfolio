59a68395c42dd5634dccd20646b86296baa15355
elaheabdali <ohelahe@gmail.com>
2023-10-07 22:11:10 +0330

    add section

--- components/section-divider.tsx
+++ components/section-divider.tsx
@@ -8,7 +8,7 @@ const SectionDivider = () => {
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.125 }}
       className='bg-gray-200 my-24 h-16 w-1 rounded-full
-    hidden sm:block'
+    hidden sm:block dark:bg-opacity-20'
     ></motion.div>
   );
 };
