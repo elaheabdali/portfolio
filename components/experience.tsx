a28bbb0f77e68b92097ca5fa56354169f3c49271
elaheabdali <ohelahe@gmail.com>
2023-10-07 22:09:57 +0330

    added

--- components/experience.tsx
+++ components/experience.tsx
@@ -5,11 +5,11 @@ import SectionHeading from './section-heading';
 import {
   VerticalTimeline,
   VerticalTimelineElement,
-} from "react-vertical-timeline-component";
+} from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import { experiencesData } from '@/lib/data';
-import { useSectionInView } from '@/lib/hooks';
-import { useTheme } from '@/context/theme-context';
+import { experiencesData } from '../lib/data';
+import { useSectionInView } from '../lib/hooks';
+import { useTheme } from '../context/theme-context';
 
 const Experience = () => {
   const { ref } = useSectionInView('Experience');
